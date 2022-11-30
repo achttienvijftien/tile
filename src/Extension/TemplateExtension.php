af67f1ee44bb7167bb2ab3c8f2c612f7990eab7f
Tobias van Dijk <tobias@1815.nl>
2022-11-30 21:12:30 +0100

    style: add docblocks

--- src/Extension/TemplateExtension.php
+++ src/Extension/TemplateExtension.php
@@ -1,12 +1,26 @@
 <?php
+/**
+ * TemplateExtension
+ *
+ * @package AchttienVijftien\Tile\Extension
+ */
 
 namespace AchttienVijftien\Tile\Extension;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
+/**
+ * Class TemplateExtension
+ */
 class TemplateExtension extends AbstractExtension {
-	public function getFunctions() {
+
+	/**
+	 * Returns twig functions provided by the extension.
+	 *
+	 * @return TwigFunction[]
+	 */
+	public function getFunctions(): array {
 		return [
 			new TwigFunction( 'wp_head', 'wp_head' ),
 			new TwigFunction( 'wp_footer', 'wp_footer' ),
